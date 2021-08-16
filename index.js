617c23496bbf8961dc6850d2f3a2f4103efe171d
Fansy <418536538@qq.com>
2021-08-17 00:32:31 +0800

    feat: new prettier config

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: false,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: "avoid",
+	vueIndentScriptAndStyle: false,
+	endOfLine: "lf",
+};
